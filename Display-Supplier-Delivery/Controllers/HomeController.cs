4795e75ab2c193a5e30821d386c0fd1dd03fc89b
Eakarin.P <eakarin.pint@gmail.com>
2025-11-23 12:24:40 +0700

    commit first

---
+++ Display-Supplier-Delivery/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Display_Supplier_Delivery.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Display_Supplier_Delivery.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Display-Supplier-Delivery/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Display_Supplier_Delivery.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
